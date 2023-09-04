04a360988cb1c60c63a7ca6a5811253c5b927a84
Fred Baube <fbaube@iki.fi>
2023-09-04 11:06:27 +0300

    Yup

---
+++ buildinsertstmt.go
@@ -0,0 +1,161 @@
+package sqlite
+
+import (
+	"fmt"
+	D "github.com/fbaube/dsmnd"
+	// FU "github.com/fbaube/fileutils"
+	RU "github.com/fbaube/repoutils"
+	S "strings"
+	// "time"
+)
+
+/* REF
+https://www.sqlite.org/lang_createtable.html
+Create Table [ If Not Exists ] [schemaname.]tablename
+1) as select-statement
+2) ( columndef,+ tableconstraint,* )
+[ table-options ] ;
+*/
+
+/* REF
+TABL Fundatype = "tabl" // This datum describes a table !
+INTG = "1234" // "INT"   // SQLITE_INTEGER 1
+FLOT = "1.0f" // "FLOAT" // SQLITE_FLOAT   2
+TEXT = "AaZz" // "TEXT"  // SQLITE_TEXT    3
+BLOB = "blob" // "BLOB"  // SQLITE_BLOB    4
+NULL = "null" // "NULL"  // SQLITE_NULL    5
+PKEY = "pkey" // PRIMARY KEY (SQLite "INTEGER")
+FKEY = "fkey" // FOREIGN KEY (SQLite "INTEGER")
+LIST = "list" // table type, one per enumeration ??
+OTHR = "othr"
+*/
+
+// BuildInsertStmt implements
+// https://www.sqlite.org/lang_insert.html
+// 
+// INSERT INTO table [ ( column-name,+ ) ] VALUES(...);
+//
+// This creates one or more new rows in an existing table:
+//  - If the column-name list after table-name is omitted, 
+//    then the number of values inserted into each row must 
+//    be the same as the number of columns in the table. 
+//  - If a column-name list is specified, then the number 
+//    of values in each term of the VALUE list must match 
+//    the number of specified columns. 
+//
+// Table columns that do not appear in the column list are populated 
+// with the default column value (specified as part of the CREATE 
+// TABLE statement), or with NULL if no default value is specified.
+// .
+func (pSR *SqliteRepo) BuildInsertStmt(pTD *RU.TableDescriptor) (string, error) {
+	var sb, sb2 S.Builder
+	panic("FIXME")
+	sb.WriteString(fmt.Sprintf("CREATE TABLE %s(\n", pTD.Name))
+	sb2.WriteString("reposqlite.GenCreTblStmt: ")
+	for _, pCS := range pTD.ColumnSpecs {
+		cnm := pCS.StorName // column name
+		fdt := pCS.Fundatype
+		sb2.WriteString(fmt.Sprintf("%s:%s, ", cnm, fdt))
+	}
+	fmt.Printf(sb2.String() + "\n")
+
+	// PRIMARY KEY IS ASSUMED - DO IT FIRST
+	// idx_mytable integer not null primary key autoincrement,
+	sb.WriteString(pTD.IDName +
+		" integer not null primary key autoincrement, " +
+		"-- NOTE: integer, not int. \n")
+
+	for _, pCS := range pTD.ColumnSpecs {
+		colName := pCS.StorName // column name in DB
+		fmt.Sprintf("Creating column: %s \n", pCS.String())
+		switch pCS.Fundatype {
+		case D.PKEY:
+			panic("DUPE PRIMARY KEY")
+
+		case D.FKEY:
+			//> D.ColumnSpec{D.FKEY, "idx_inbatch", "inbatch",
+			//>  "Input batch of imported content"},
+			// referencing fields's name is idx_inbatch
+			refgField := colName
+			// referenced table's name is inbatch
+			refdTable := pCS.DispName
+
+			// Count up underscores (per comment above)
+			ss := S.Split(refgField, "_")
+			switch len(ss) {
+
+			case 2: // normal case, for example:
+				//> idx_inbatch integer not null ref's inbatch,
+				//> foreign key(idx_inbatch) references
+				//>     inbatch(idx_inbatch)
+				sb.WriteString(fmt.Sprintf(
+					"%s integer not null references %s,\n",
+					refgField, refdTable))
+				sb.WriteString(fmt.Sprintf(
+					"foreign key(%s) references %s(%s),\n",
+					refgField, refdTable, refgField))
+			case 3: // multiple indices into same table, e.g.
+				//> idx_cnt_map integer not null ref's cont'y,
+				//> frn key(idx_cnt_map) refs cont'y(idx_cont'y),
+				//> idx_cnt_tpc integer not null ref's cont'y,
+				//> frn key(idx_cnt_tpc) refs cont'y(idx_cont'y),
+				// We have to deduce "idx_contentity", which
+				// we can do confidently after passing checks.
+				var refdField string
+				if S.EqualFold(ss[0], "idx") &&
+					S.EqualFold(ss[1][0:1], refdTable[0:1]) {
+					refdField = "idx_" + refdTable
+					sb.WriteString(fmt.Sprintf(refgField+
+						" integer not null "+
+						"references %s,\n", refdTable))
+					sb.WriteString(fmt.Sprintf("foreign "+
+						"key(%s) references %s(%s),\n",
+						refgField, refdTable, refdField))
+				} else {
+					return "", fmt.Errorf(
+						"Malformed a_b_c FKEY: %s,%s,%s",
+						refgField, refdTable, refdField)
+				}
+			default:
+				return "", fmt.Errorf("Malformed FKEY: "+
+					"%s,%s,%s", refgField, refdTable)
+			}
+		case D.TEXT:
+			sb.WriteString(colName + " text not null,\n")
+		case D.INTG:
+			// filect int not null check (filect >= 0) default 0
+			sb.WriteString(colName + " int not null,\n")
+		/* Unimplem'd:
+		case D.FLOT:
+		case D.BLOB:
+		case D.NULL:
+		case D.LIST:
+		case D.OTHR:
+		*/
+		default:
+			panic(pCS.Fundatype)
+		}
+	}
+	// trim off final ",\n"
+	ss := sb.String()
+	sb3 := ss[0:len(ss)-2] + "\n) STRICT;\n"
+	return sb3, nil
+}
+
+/*
+CREATE TABLE contentity( -- STRICT!
+idx_inbatch integer not null references inbatch,
+relfp text not null check (typeof(relfp) == 'text'),
+absfp text not null check (typeof(absfp) == 'text'),
+t_cre text not null check (typeof(t_cre) == 'text'),
+t_imp text not null check (typeof(t_imp) == 'text'),
+t_edt text not null check (typeof(t_edt) == 'text'),
+descr text not null check (typeof(descr) == 'text'),
+mimetype text not null check (typeof(mimetype) == 'text'),
+mtype     text not null check (typeof(mtype)     == 'text'),
+xmlcontype text not null check (typeof(xmlcontype) == 'text'),
+ditaflavor text not null check (typeof(ditaflavor) == 'text'),
+ditacontype text not null check (typeof(ditacontype) == 'text'),
+foreign key(idx_inbatch) references inbatch(idx_inbatch)
+);
+*/
---
+++ go.mod
@@ -0,0 +1,32 @@
+module github.com/fbaube/reposqlite
+
+go 1.20
+
+require (
+	github.com/fbaube/dsmnd v0.0.0-20230622205413-6095fd80db31
+	github.com/fbaube/fileutils v0.0.0-20230623071738-0db5b6a18847
+	github.com/fbaube/mlog v0.0.0-20230205182808-c77321da3ff3
+	github.com/fbaube/repo v0.0.0-20230702193159-4a0df3ed961a
+	github.com/fbaube/repoutils v0.0.0-20230623084640-e4530f5028d4
+	github.com/fbaube/rowmodels v0.0.0-20230622080238-d1b28a526896
+	github.com/fbaube/sqlite3 v0.0.0-20230623201839-76cebbc048ba
+	github.com/fbaube/stringutils v0.0.0-20230622210105-a1af04c1f93d
+)
+
+require (
+	github.com/fatih/color v1.15.0 // indirect
+	github.com/fbaube/ctoken v0.0.0-20230518060940-6f364ff08c9d // indirect
+	github.com/fbaube/logutils v0.0.0-20230623084753-053ea535035d // indirect
+	github.com/fbaube/miscutils v0.0.0-20230702174835-89345331d87b // indirect
+	github.com/fbaube/wasmutils v0.0.0-20230623084957-b4f528abb99e // indirect
+	github.com/fbaube/xmlutils v0.0.0-20230518061403-2465e549e10a // indirect
+	github.com/gabriel-vasile/mimetype v1.4.2 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.19 // indirect
+	github.com/mgutz/str v1.2.0 // indirect
+	golang.org/x/exp v0.0.0-20230817173708-d852ddb80c63 // indirect
+	golang.org/x/net v0.14.0 // indirect
+	golang.org/x/sys v0.12.0 // indirect
+	golang.org/x/tools v0.12.1-0.20230815132531-74c255bcf846 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+)
